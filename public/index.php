d6bc35897766072e9b89fa8552556d21244b8605
Kirill Dyatlov <kirill.dyatlov.webdeveloper@yandex.ru>
2021-09-25 06:43:05 +0300

    kernel separate load resources

--- public/index.php
+++ public/index.php
@@ -2,18 +2,10 @@
 
 declare(strict_types=1);
 
-use App\System\Router;
+use App\System\Kernel;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-Router::add('/', Router::GET, 'App\Controller\HomepageController::index');
-
-// Tasks
-Router::add('/task/(\d+)/edit', Router::GET, 'App\Controller\TasksController::edit');
-Router::add('/task/(\d+)/show', Router::GET, 'App\Controller\TasksController::show');
-
-// Security
-Router::add('/login', Router::GET, 'App\Controller\SecurityController::login');
-Router::add('/logout', Router::GET, 'App\Controller\SecurityController::logout');
-
-Router::run();
+$kernel = new Kernel();
+$kernel->boot();
+global $kernel;
--- src/System/Kernel.php
+++ src/System/Kernel.php
@@ -6,13 +6,34 @@ namespace App\System;
 
 class Kernel
 {
+    private array $db;
+
     public function boot(): void
     {
-        echo "booted";
+        $this->loadConfig();
+        $this->loadRouters();
     }
 
     public static function getRootDir(): string
     {
         return __DIR__ . '/../../';
+    }
+
+    public function getDbConfig(): array
+    {
+        return $this->db;
+    }
+
+    private function loadConfig(): void
+    {
+        $root = self::getRootDir();
+        $db = require_once $root . '/config/dbase.php';
+        $this->db = $db;
+    }
+
+    private function loadRouters(): void
+    {
+        $root = self::getRootDir();
+        require_once $root . '/config/routers.php';
     }
 }
